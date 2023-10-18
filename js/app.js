e6667e24cdc8cd65457fb9c931540591ccc5209b
Limson91 <matteo.a.colautti@gmail.com>
2023-10-18 18:20:06 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const teamCrew = {
+    name : 'Angela Carroll',
+    job : 'Chief Editor',
+    photo : 'angela-caroll-chief-editor.jpg',
+
+    name : 'Angela Lopez',
+    job : 'Social Media Manager',
+    photo : 'angela-lopez-social-media-manager.jpg',
+
+    name : 'Barbara Ramos',
+    job : 'Graphic Designer',
+    photo : 'barbara-ramos-graphic-designer.jpg',
+
+    name : 'Scott Estrada',
+    job : 'Developer',
+    photo : 'scott-estrada-developer.jpg',
+
+    name : 'Walter Gordon',
+    job : 'Office Manager',
+    photo : 'walter-gordon-office-manager.jpg',
+
+    name : 'Wayne Barnett',
+    job : 'Founder & CEO',
+    photo : 'wayne-barnett-founder-ceo.jpg',
+}
